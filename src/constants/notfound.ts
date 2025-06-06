aae0b5807a902133d7ceac24084f0286048da005
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-06-06 16:54:10 +0300

    refactor: add not found interface

--- src/constants/notfound.ts
+++ src/constants/notfound.ts
@@ -1,4 +1,11 @@
-export const notfound = {
+interface NotFound {
+  title: string;
+  description: string;
+  status: number;
+  home: string;
+}
+
+export const notfound: NotFound = {
   title: "Oops! We can't find that page.",
   description: "The page you are looking for doesn't exist or has been moved.",
   status: 404,
